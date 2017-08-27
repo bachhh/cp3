483b2e5b3fe029e90c0b6da56e31b5feec0f2e62
k-i-k-ichi <k-i-k-ichi@users.noreply.github.com>
2017-08-27 18:35:16 +0700

    cf 236 A

---
+++ cf236a.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <stdio.h>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <map>
+#include <sstream>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <iterator>
+#include <bitset>
+#include <algorithm>
+
+using namespace std;
+
+typedef long int int32;
+typedef long long int int64;
+typedef unsigned long int uint32;
+typedef unsigned long long int uint64;
+
+const double pi = acos(-1.0);
+
+#define PI 3.1415926535897932384626433832795
+#define INF (int)1e9
+#define FOREACH(it, l) for (auto it = l.begin(); it != l.end(); it++)
+
+
+int main(){
+
+  //freopen("input.txt","r", stdin);
+  //freopen("output.txt","w", stdout);
+  map<char, bool> hashmap;
+  string username;
+  cin >> username;
+  for (char &c : username){
+    hashmap[c] = true;
+  } 
+  
+  if (!(hashmap.size() % 2)) cout << "CHAT WITH HER!";
+  else cout << "IGNORE HIM!";
+
+  return 0;
+}
