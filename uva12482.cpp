5c6682ee1e54aa604c02af64fd2d5c9e276f87a6
k-i-k-ichi <k-i-k-ichi@users.noreply.github.com>
2017-11-13 10:15:59 +0700

    \'UVA12482.cpp\'

---
+++ uva12482.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <assert.h>
+#include <iostream>
+#include <assert.h>
+#include <stdio.h>
+#include <cmath>
+#include <string>
+#include <string.h>
+#include <vector>
+#include <map>
+#include <list>
+#include <unordered_map>
+#include <sstream>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <iterator>
+#include <bitset>
+#include <algorithm>
+#include <tuple>
+#include <functional>
+#include <ctime>
+
+using namespace std;
+typedef long int int32;
+typedef long long int int64;
+typedef unsigned long int uint32;
+typedef unsigned long long int uint64;
+const double pi = acos(-1.0);
+
+#define PI 3.1415926535897932384626433832795
+#define INF (int)1e9
+#define FOREACH(it, l) for (auto it = l.begin(); it != l.end(); it++)
+#define LOOP(n) for (int z = 0; z < n; ++z)
+#define MAX(a, b) ( (a > b ) ? a : b )
+#define MIN(a, b) ( (a < b ) ? a : b )
+
+void swap(int* a, int *b){
+  *a ^= *b;
+  *b ^= *a;
+  *a ^= *b;
+}
+
+//  ***** MAIN *****
+int main(){
+  ios::sync_with_stdio(false);
+  int l, c, n, lines,pages, chars;
+  string w;
+  while( cin >> n >> l >> c){
+    chars = 0;lines = 1, pages = 1;
+    LOOP(n){
+      cin >> w;
+      if ( (chars + w.size()) > c){
+        lines++;
+        if (lines > l){
+          pages++;
+          lines = 1;
+        }
+        chars = w.size()+1;
+      }
+      else {
+        chars += w.size();
+        chars++;
+      }
+    }
+    cout << pages << endl;
+  }
+
+  return 0;
+}
+
+
