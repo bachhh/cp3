f1176129fdb58d5d72cdb56f9baf1fc2b71e9a72
k-i-k-ichi <k-i-k-ichi@users.noreply.github.com>
2017-12-03 19:21:58 +0700

    \'UVA11953.cpp\'

--- uva11953.cpp
+++ uva11953.cpp
@@ -1,88 +1,149 @@
-#include <iostream>
-#include <assert.h>
-#include <iostream>
-#include <assert.h>
-#include <stdio.h>
-#include <cmath>
-#include <string>
-#include <string.h>
-#include <vector>
-#include <map>
-#include <list>
-#include <unordered_map>
-#include <sstream>
-#include <stack>
-#include <queue>
-#include <deque>
-#include <iterator>
-#include <bitset>
-#include <algorithm>
-#include <tuple>
-#include <functional>
-#include <ctime>
+#include <bits/stdc++.h>
 
 using namespace std;
-typedef long int int32;
-typedef long long int int64;
-typedef unsigned long int uint32;
-typedef unsigned long long int uint64;
 const double pi = acos(-1.0);
 
-#define PI 3.1415926535897932384626433832795
+typedef long int int32; typedef long long int int64;
+typedef unsigned long int uint32; typedef unsigned long long int uint64;
+typedef pair<int, int> ii; typedef vector<ii> vii; typedef vector<int> vi;
+typedef vector<vi> vvi;
+
 #define INF (int)1e9
+#define PI 3.1415926535897932384626433832795
+#define VISITED 1
+#define UNVISITED -1
+
 #define FOREACH(it, l) for (auto it = l.begin(); it != l.end(); it++)
-#define LOOP(n) for (int z = 0; z < n; ++z)
+#define LOOP(i, n) for (int i = 0; i < n; ++i)
 #define MAX(a, b) ( (a > b ) ? a : b )
 #define MIN(a, b) ( (a < b ) ? a : b )
+#define DEBUG(x) cerr << #x << " is " << x << endl;
 
-void swap(int* a, int *b){
+
+void swap(int32* a, int32* b){
   *a ^= *b;
   *b ^= *a;
   *a ^= *b;
 }
 
-//  ***** MAIN *****
-int main(){
-  //ios::sync_with_stdio(false);
-  int p_milage, milage;
-  string event;
-  double fuel_cap = 0, leak = 0, fuel_eco, max_fuel_cap = 0;
-
-  while(true){
-    cin >> milage >> event;
-    if(milage == 0 && event == "Fuel"){
-      cin >> event >> fuel_eco;
-      fuel_eco /= 100;
-      if (fuel_eco == 0){
-        break;
-      }
-    }
-    else{
-      fuel_cap += fuel_eco * (milage - p_milage) + leak *(milage - p_milage);
-      max_fuel_cap = MAX(max_fuel_cap, fuel_cap);
-      if (event == "Goal"){
-        printf("%.3f\n", max_fuel_cap);
-        leak = fuel_cap = max_fuel_cap = 0;
-      }
-      else if (event == "Fuel"){
-        cin >> event >> fuel_eco;
-        fuel_eco /= 100.00;
-      }
-      else if (event == "Mechanic"){
-        leak = 0;
-      }
-      else if (event == "Gas"){
-        cin >> event;
-        fuel_cap = 0;
-      }
-      else if (event == "Leak"){
-        leak++;
-      }
-    }
-    printf("Total %0.3f | %.3f x %.3f\n", fuel_cap, fuel_eco + leak, milage - p_milage);
-    p_milage = milage;
-  }
-
-  return 0;
+template < typename F, typename S >
+void debug(const pair< F, S > & p ) {
+  cout << "(" << p.first << ", " << p.second << ")";
 }
 
+template < typename T >
+void debug(const vector< T > &v ) {
+  typename vector< T > :: const_iterator it;
+  cout << "{ ";
+  for(int i =0; i < v.size(); ++i){
+    cout << v[i] <<  ", "[i == (v.size()-1)];
+  }
+  cout << " }" << endl;
+}
+
+template < typename T >
+void debug( const set< T > &v ) {
+  cout << "[";
+  typename set< T > :: const_iterator it;
+  for ( it = v.begin(); it != v.end(); it++ ) {
+      if( it != v.begin() ) cout << ", ";
+        cout << *it;
+  }
+  cout << "]";
+}
+
+template < typename F, typename S >
+void debug( const map< F, S > &v ) {
+  cout << "[";
+  typename map< F , S >::const_iterator it;
+  for( it = v.begin(); it != v.end(); it++ ) {
+    if( it != v.begin() ) cout << ", ";
+      cout << it -> first << " = " << it -> second ;
+  }
+  cout << "]";
+}
+
+template < typename T >
+void debug( const vector< vector< T > > &v ) {
+  cout << "\n";
+  typename vector< vector< T > > :: const_iterator row;
+  typename vector< T > :: const_iterator col;
+  for( row = v.begin(); row != v.end(); ++row ) {
+    for( col = (*row).begin(); col != (*row).end(); ++col) {
+      cout << *col << " \n"[col == prev((*row).end())];
+    }
+  }
+  cout << "\n";
+}
+
+void debug( const vii &v ) {
+  size_t s = v.size();
+  for(int i = 0; i < s; ++i){
+    cout << "(" << v[i].first + 1 << ", " << v[i].second << ")" << " \n"[i == (s-1)];
+  }
+}
+
+void debug( const vector< vii > &v ) {
+  for(int i = 0; i < v.size(); ++i){
+    cout << i+1 << " | ";
+    for( int j = 0; j < v[i].size(); ++j ) {
+      cout << "(" << v[i][j].first + 1 << ", " << v[i][j].second << ")"
+        << " \n"[j == v[i].size()-1];
+    }
+  }
+  cout << "\n";
+}
+
+template < typename T>
+void printM(T *matrix, size_t row, size_t col){
+  cout << "\n";
+  for(int i = 0; i < row; ++i){
+    for(int j = 0; j < col; ++j){
+      cout << matrix[i][j] << " \n"[j == col-1];
+    }
+  }
+  cout << "\n";
+}
+
+
+int output, n, tc;
+char grid[100][100];
+
+int moves[5] = {1, 0, -1, 0, 1};
+
+void dfs(int x, int y){
+  if ( x < 0 || y < 0 || x >= n || y >= n) return;
+  if ( grid[x][y] == '.') return;
+
+  grid[x][y] = '.';
+  for(int i = 0; i < 4; ++i){
+    dfs(x + moves[i], y + moves[i+1]);
+  }
+}
+
+//  ***** MAIN *****
+int main(){
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  cin >> tc;
+  for(int ca= 1; ca <= tc; ++ca){
+    cin >> n;
+    output = 0;
+    for(int i = 0; i < n; ++i){
+      for(int j = 0; j < n; ++j){
+        cin >> grid[i][j];
+      }
+    }
+    for(int i = 0; i < n; ++i){
+      for(int j = 0; j < n; ++j){
+        if (grid[i][j] == 'x'){
+          dfs(i, j);
+          output++;
+        }
+      }
+    }
+    printf("Case %d: %d\n",ca, output);
+  }
+  return 0;
+}
