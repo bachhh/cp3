40ba4165db8fd944c9d43aac5583dd9ef456f7ab
k-i-k-ichi <k-i-k-ichi@users.noreply.github.com>
2017-12-23 20:06:55 +0700

    \'cf385a.cpp\'

--- cf385a.cpp
+++ cf385a.cpp
@@ -1,25 +1,32 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-const double pi = acos(-1.0);
-
-typedef long int int32; typedef long long int int64;
-typedef unsigned long int uint32; typedef unsigned long long int uint64;
-typedef pair<int, int> ii; typedef vector<ii> vii; typedef vector<int> vi;
-typedef tuple<int, int, int> iii; typedef tuple<int, int, int, int> iiii;
-typedef vector<vi> vvi;
 
 #define MAX(a, b) ( (a > b ) ? a : b )
 #define MIN(a, b) ( (a < b ) ? a : b )
+
 #define LIMIT 101
-#define INF INT_MAX
 
 //  ***** MAIN *****
 int main(){
   //ios::sync_with_stdio(false);
-  //cin.tie(NULL);
+  cin.tie(NULL);
 
-  int
+  int n, c, m = 0;
+  cin >> n >> c;
+  int xs[LIMIT];
+  for (int i = 0; i < n; i++) {
+    cin >> xs[i];
+  }
+
+  for (int i = 0; i <n-1 ; i++) {
+    m = MAX(m, (xs[i]-xs[i+1])-c);
+  }
+  std::cout << m << std::endl;
+
+
 
   return 0;
+
 }
+
