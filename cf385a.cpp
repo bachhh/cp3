a34579e22bf0a36091e38b05e2db12cda8c25d17
k-i-k-ichi <k-i-k-ichi@users.noreply.github.com>
2017-12-23 20:06:34 +0700

    'CF385a.cpp'

---
+++ cf385a.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+const double pi = acos(-1.0);
+
+typedef long int int32; typedef long long int int64;
+typedef unsigned long int uint32; typedef unsigned long long int uint64;
+typedef pair<int, int> ii; typedef vector<ii> vii; typedef vector<int> vi;
+typedef tuple<int, int, int> iii; typedef tuple<int, int, int, int> iiii;
+typedef vector<vi> vvi;
+
+#define MAX(a, b) ( (a > b ) ? a : b )
+#define MIN(a, b) ( (a < b ) ? a : b )
+#define LIMIT 101
+#define INF INT_MAX
+
+//  ***** MAIN *****
+int main(){
+  //ios::sync_with_stdio(false);
+  //cin.tie(NULL);
+
+  int
+
+  return 0;
+}
