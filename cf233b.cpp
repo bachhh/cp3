6c05c0502307af9c382a5a1c108503cded6c3911
k-i-k-ichi <k-i-k-ichi@users.noreply.github.com>
2018-01-02 17:48:27 +0700

    cf233

--- cf233b.cpp
+++ cf233b.cpp
@@ -2,43 +2,52 @@
 
 using namespace std;
 
-#define MAX(a, b) ( (a > b ) ? a : b )
-#define MIN(a, b) ( (a < b ) ? a : b )
 
 #define LIMIT 100
 
-int sumDigit(int64_t x){
+int sumDig(int64_t a){
   int r = 0;
-  while(x>0){
-    r += x%10;
-    x /= 10;
+  while(a>0){
+    r += a%10;
+    a /= 10;
   }
   return r;
 }
-
 //  ***** MAIN *****
 int main(){
   //ios::sync_with_stdio(false);
   cin.tie(NULL);
 
   int64_t n; cin >> n;
+  int64_t sx;
 
-  int64_t high = n;
-  int64_t low = 1;
-  int64_t mid;
+  int64_t v[91] = {-1};
 
-  while(high > low){
-    mid = low+ (high-low)/2;
-    if ((pow(mid,2) + sumDigit(mid)*mid) >= n ){
-      high = mid;
-    }
-    else {
-      low = mid+1;
+  // delta = sx^2 + 4*n
+  for (sx = 1; sx < 90; ++sx){
+    int64_t dsqrt = sqrt(sx*sx+4*n);
+    if(dsqrt*dsqrt == sx*sx+4*n){
+      int64_t roottwice = (-sx+dsqrt);
+      if( roottwice%2 == 0 && sumDig(roottwice/2) == sx ){
+        v[sx] = roottwice/2;
+      }
     }
   }
-  low = 9;
-  std::cout << pow(low,2) + sumDigit(low)*low << std::endl;
-  std::cout << low << std::endl;
+  int64_t output = LLONG_MAX;
+
+  for (int i = 0; i < 91; i++) {
+    if(v[i] > 0){
+      output = min(output, v[i]);
+    }
+  }
+
+  if(output < LLONG_MAX){
+    std::cout << output << std::endl;
+  }
+  else{
+    std::cout << -1 << std::endl;
+  }
+
 
   return 0;
 }
