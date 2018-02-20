72f0b7d0bad6557e1732d569cdf441c6ac121368
k-i-k-ichi <k-i-k-ichi@users.noreply.github.com>
2018-02-20 14:21:03 +0700

    cf171

---
+++ cf171e.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define LIMIT 100
+typedef pair<int64_t, int64_t> ii;
+
+//  ***** MAIN *****
+int main(){
+  //ios::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  return 0;
+}
+
