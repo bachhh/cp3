46394d6a591941af561096cecc02e012c056d6ba
k-i-k-ichi <k-i-k-ichi@users.noreply.github.com>
2018-03-08 15:27:50 +0700

    abc

--- abc77b.cpp
+++ abc77b.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 #define LIMIT 100
-typedef long long ll;
+typedef int64_t ll;
 typedef pair<ll, ll> ii;
 int gcd(ll a, ll b) { return (b==0)?a:gcd(b, a%b); }
 int lcm(ll a, ll b) { ll i=(a/gcd(a,b))*b; assert(i>0); return i; }
@@ -12,6 +12,8 @@ int lcm(ll a, ll b) { ll i=(a/gcd(a,b))*b; assert(i>0); return i; }
 int main(){
   //ios::sync_with_stdio(false);
   cin.tie(NULL);
+  int n; cin >> n;
+  ::cout << (int)floor(sqrt(n))*(int)floor(sqrt(n))<< std::endl;
 
   return 0;
 }
