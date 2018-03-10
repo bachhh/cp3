b12b8aad03b278dbea7aa04779b7c31a9b3e8785
k-i-k-ichi <k-i-k-ichi@users.noreply.github.com>
2018-03-10 16:24:31 +0700

    cf233b.cpp

--- cf233b.cpp
+++ cf233b.cpp
@@ -2,52 +2,53 @@
 
 using namespace std;
 
-
 #define LIMIT 100
+typedef long long ll;
+typedef pair<ll, ll> ii;
+int gcd(ll a, ll b) { return (b==0)?a:gcd(b, a%b); }
+int lcm(ll a, ll b) { ll i=(a/gcd(a,b))*b; assert(i>0); return i; }
 
-int sumDig(int64_t a){
-  int r = 0;
-  while(a>0){
-    r += a%10;
-    a /= 10;
-  }
-  return r;
+ll compute(ll x, ll s ){
+  return x*x + s*x;
 }
+
+ll sumOfDigit(ll x){
+  int o =0 ;
+  while(x){ o+= x%10; x/=10;}
+  return o;
+}
+
 //  ***** MAIN *****
 int main(){
   //ios::sync_with_stdio(false);
   cin.tie(NULL);
 
-  int64_t n; cin >> n;
-  int64_t sx;
+  ll n; cin >> n;
 
-  int64_t v[91] = {-1};
+  for (int s = 1; s <= 9*17; ++s) {
 
-  // delta = sx^2 + 4*n
-  for (sx = 1; sx < 90; ++sx){
-    int64_t dsqrt = sqrt(sx*sx+4*n);
-    if(dsqrt*dsqrt == sx*sx+4*n){
-      int64_t roottwice = (-sx+dsqrt);
-      if( roottwice%2 == 0 && sumDig(roottwice/2) == sx ){
-        v[sx] = roottwice/2;
+    ll l = 0, h = ceil(sqrt(n))*2;
+    ll ret, m;
+    while(l <= h){
+      m = l + (h-l)/2;
+      ret = compute(m, s);
+      if(ret < n){
+        l = m+1;
+      }
+      else if(ret > n){
+        h = m-1;
+      }
+      else {
+        break;
       }
     }
-  }
-  int64_t output = LLONG_MAX;
 
-  for (int i = 0; i < 91; i++) {
-    if(v[i] > 0){
-      output = min(output, v[i]);
+    if(sumOfDigit(m) == s && ret == n){
+      std::cout << m << std::endl;
+      return 0;
     }
   }
-
-  if(output < LLONG_MAX){
-    std::cout << output << std::endl;
-  }
-  else{
-    std::cout << -1 << std::endl;
-  }
-
+  std::cout << -1 << std::endl;
 
   return 0;
 }
